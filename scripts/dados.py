79126300b5d43da985378cc48312ca4d96cc4c46
Felipe Gonzalez 🇧🇷 🇺🇸 <felipedelavega08@gmail.com>
2026-05-15 21:11:14 -0300

    Create dados.py

---
+++ scripts/dados.py
@@ -0,0 +1,8 @@
+# ALL CANDIDATES ARE FICTITICIUS / TODOS OS CANDIDATOS SÃO FICTÍCIOS
+
+candidatos = [
+    {'candidato': 'FELIPE', 'votos': 0},
+    {'candidato': 'GONZALEZ', 'votos': 0},
+    {'candidato': 'MAURICIO', 'votos': 0},
+    {'candidato': 'LUIZ', 'votos': 0},
+]
